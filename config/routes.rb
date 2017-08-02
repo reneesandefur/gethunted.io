e7486e1fb80b7ecb00018241c9bb9615f6a1551f
Renee Sandefur <reneersandefur@gmail.com>
2017-08-02 03:09:03 +0000

    favicon and non essential changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'emails#index'
   resources :emails, only: [:new, :create]
+  resources :static_pages, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
